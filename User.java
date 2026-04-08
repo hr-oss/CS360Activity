2060878594cabfa7caf884ea03ddbc326620fc97
Pryce <146033887+raigeki2k4@users.noreply.github.com>
2026-04-08 15:39:29 -0400

    Creation of user class which Student, Admin, and Tutor will extend.

---
+++ User.java
@@ -0,0 +1,36 @@
+public class User {
+    private static int userCount = 0;
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        userCount++;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public static int getUserCount() {
+        return userCount;
+    }
+    
+    public void setPassword(String newPassword) {
+        this.password = newPassword;
+    }
+
+    public void setUsername(String newUsername) {
+        this.username = newUsername;
+    }
+
+    @Override
+    public String toString() {
+        return "Username: " + username + "\nPassword: " + password;
+    }
+}
